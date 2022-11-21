11c34ca6eff4eadbfdcab4112635dc34b1b95414
fronzen100826 <cmgg919@gmail.com>
2022-11-21 15:40:13 -0500

    modified

--- map.js
+++ map.js
@@ -1,10 +1,35 @@
 const words = ["ground", "control", "to", "major", "tom"];
 
-const map = function(array, callback) {
-  // temporary code:
-  console.log('array: ', array);
-  console.log('callback: ', callback);
+const assertArraysEqual = function(array1, array2)
+{
+  if(array1.length === array2.length)
+  {
+    for (var i = 0; i < array1.length; i ++)
+    {
+      if(array1[i] !== array2[i])
+      {
+        console.log("Arrays are not the same");
+        return false;
+      }
+    }
 
+  }
+  else
+  {
+console.log("arrays not same length, therefore not the same");
+    return false;
+  }
+console.log("arrays are the same!");
+return true;
+
+
+}
+const map = function(array, callback) {
   const results = [];
+  for (let item of array) {
+    console.log('item BEFORE: ', item);
+    console.log('item AFTER: ', callback(item));
+    console.log('---');
+  }
   return results;
 }
