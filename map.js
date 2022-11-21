320beb6076ff280a15fe4f77d640a80e043d52c3
fronzen100826 <cmgg919@gmail.com>
2022-11-21 14:18:36 -0500

    first done

---
+++ map.js
@@ -0,0 +1,10 @@
+const words = ["ground", "control", "to", "major", "tom"];
+
+const map = function(array, callback) {
+  // temporary code:
+  console.log('array: ', array);
+  console.log('callback: ', callback);
+
+  const results = [];
+  return results;
+}
