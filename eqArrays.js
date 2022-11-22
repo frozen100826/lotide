c3e25af37891a612b922683c058f0574125ac6f7
fronzen100826 <cmgg919@gmail.com>
2022-11-22 00:33:56 -0500

    refac

--- eqArrays.js
+++ eqArrays.js
@@ -1,15 +1,20 @@
-const assertEqual = require('./assertEqual');
-
 const eqArrays = function(arr1, arr2) {
-  if (arr1.length !== arr2.length)
+  if (arr1.length !== arr2.length) {
     return false;
-  for (let i = arr1.length; i--;) {
-    if (arr1[i] !== arr2[i]) {
-      return false;
+  }
+
+  for (let i = 0; i < arr1.length; i++) {
+    if (Array.isArray(arr1[i]) && Array.isArray(arr2[i])) {
+      if (!eqArrays(arr1[i], arr2[i])) {
+        return false;
+      }
+    } else if (arr1[i] !== arr2[i]) {
+      return false
     }
   }
   return true;
 };
+
 
 
 module.exports = eqArrays;
